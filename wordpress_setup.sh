be57c0c9538cae95ee676042b6b8edee59b2be70
Anggrdwjy <anggarda.wijaya410@gmail.com>
2026-01-04 14:54:40 +0700

    Create wordpress_setup.sh

---
+++ wordpress_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#By Anggarda Saputra Wijaya
+#Install Wordpress on Container
+sudo apt-get update -y
+sudo apt-get install curl software-properties-common docker.io -y
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+sudo apt-get update -y
+mkdir /home/portainer
+mkdir /home/wordpress
+mkdir /home/wordpress/wordpress_file
+mkdir /home/wordpress/db_data
+cp support/docker-portainer.yml /home/portainer/docker-compose.yml
+cp support/docker-wordpress.yml /home/wordpress/docker-compose.yml
+cd /home/portainer && docker-compose up -d
+cd /home/wordpress && docker-compose up -d
+docker ps -a
+ufw allow 22
+ufw allow 8080
+ufw allow 19443
+ufw enable
+systemctl status ufw
